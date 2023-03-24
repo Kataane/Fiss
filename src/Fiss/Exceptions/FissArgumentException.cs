47855e4f20b9a4138de8c5d74175131cfff90058
Katanovich Dmitry <dmitry.katanovich@sibers.com>
2023-03-25 00:58:26 +0700

    Change ArgumentException to ArgumentOutOfRangeException

--- src/Fiss/Exceptions/FissArgumentException.cs
+++ src/Fiss/Exceptions/FissArgumentException.cs
@@ -1,38 +1,81 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿#pragma warning disable CS8777
+
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Fiss.Exceptions;
 
+/// <summary>
+///     Represents an exception that is thrown when an argument fails validation.
+/// </summary>
 internal class FissArgumentException
 {
+    /// <summary>
+    ///     Throws an exception if <paramref name="value1" /> is greater than <paramref name="value2" />.
+    /// </summary>
+    /// <param name="value1">The first value to compare.</param>
+    /// <param name="value2">The second value to compare.</param>
+    /// <param name="value1ParamName">The name of the parameter <paramref name="value1" />.</param>
+    /// <param name="value2ParamName">The name of the parameter <paramref name="value2" />.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown when <paramref name="value1" /> is greater than <paramref name="value2" />.
+    /// </exception>
     public static void ThrowIfMoreThan(
-        [NotNull] int? value1, [NotNull] int? value2, 
-        [CallerArgumentExpression("value1")] string? value1ParamName = null, 
-        [CallerArgumentExpression("value2")] string? value2ParamName = null)
+        [NotNull] int? value1, [NotNull] int? value2,
+        [CallerArgumentExpression(nameof(value1))]
+        string? value1ParamName = null,
+        [CallerArgumentExpression(nameof(value2))]
+        string? value2ParamName = null)
     {
         if (value1 > value2)
         {
-            ThrowMoreThan(value1, value2,value1ParamName, value2ParamName);
+            ThrowMoreThan(value1, value2, value1ParamName, value2ParamName);
         }
     }
 
-    public static void ThrowIfLessZero([NotNull] int? argument, [CallerArgumentExpression("argument")] string? paramName = null)
+    /// <summary>
+    ///     Throws an exception if <paramref name="value" /> is less than zero.
+    /// </summary>
+    /// <param name="value">The value to check.</param>
+    /// <param name="paramName">The name of the parameter <paramref name="value" />.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown when <paramref name="value" /> is less than zero.
+    /// </exception>
+    public static void ThrowIfLessZero([NotNull] int? value,
+        [CallerArgumentExpression(nameof(value))] string? paramName = null)
     {
-        if (argument < 0)
+        if (value < 0)
         {
             ThrowValueLessZero(paramName);
         }
     }
 
+    /// <summary>
+    ///     Throws an exception indicating that <paramref name="value1" /> is greater than <paramref name="value2" />.
+    /// </summary>
+    /// <param name="value1">The first value that is greater than the second.</param>
+    /// <param name="value2">The second value that is less than the first.</param>
+    /// <param name="paramName">The name of the parameter <paramref name="value1" />.</param>
+    /// <param name="compareParamName">The name of the parameter <paramref name="value2" />.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown with a message indicating that <paramref name="value1" /> is greater than <paramref name="value2" />.
+    /// </exception>
     [DoesNotReturn]
     private static void ThrowMoreThan(int? value1, int? value2, string? paramName, string? compareParamName)
     {
-        throw new ArgumentException($"The {paramName} value {value1} cannot be more than {compareParamName} value {value2}.");
+        throw new ArgumentOutOfRangeException(paramName, $"The {paramName} value {value1} cannot be more than {compareParamName} value {value2}.");
     }
 
+    /// <summary>
+    ///     Throws an exception indicating that <paramref name="paramName" /> value is less than zero.
+    /// </summary>
+    /// <param name="paramName">The name of the parameter with the value less than zero.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    ///     Thrown with a message indicating that the value for <paramref name="paramName" /> cannot be less than zero.
+    /// </exception>
     [DoesNotReturn]
     private static void ThrowValueLessZero(string? paramName)
     {
-        throw new ArgumentException($"The {paramName} value cannot be less than 0.");
+        throw new ArgumentOutOfRangeException(paramName, $"The {paramName} value cannot be less than 0.");
     }
 }
